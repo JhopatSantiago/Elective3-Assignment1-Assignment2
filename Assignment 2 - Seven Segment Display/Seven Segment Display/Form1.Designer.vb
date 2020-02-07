e188b97bcea8fc89457b4c10d9ea2e738ca9d7aa
JhopatSantiago <46297312+JhopatSantiago@users.noreply.github.com>
2020-02-07 20:38:47 +0800

    Change name of the folder

---
+++ Assignment 2 - Seven Segment Display/Seven Segment Display/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.Transparent
+        Me.Button1.Location = New System.Drawing.Point(100, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(227, 44)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.Transparent
+        Me.Button2.Location = New System.Drawing.Point(53, 74)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(42, 173)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.Transparent
+        Me.Button3.Location = New System.Drawing.Point(333, 74)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(41, 173)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.Button4.Enabled = False
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.ForeColor = System.Drawing.Color.Transparent
+        Me.Button4.Location = New System.Drawing.Point(100, 245)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(227, 45)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.Button5.Enabled = False
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.ForeColor = System.Drawing.Color.Transparent
+        Me.Button5.Location = New System.Drawing.Point(100, 443)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(227, 46)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.Button6.Enabled = False
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.ForeColor = System.Drawing.Color.Transparent
+        Me.Button6.Location = New System.Drawing.Point(333, 289)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(41, 157)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.Button7.Enabled = False
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.ForeColor = System.Drawing.Color.Transparent
+        Me.Button7.Location = New System.Drawing.Point(53, 289)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(41, 157)
+        Me.Button7.TabIndex = 6
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Silver
+        Me.Label1.Location = New System.Drawing.Point(95, 515)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(238, 28)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Seven Segment Counter"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Silver
+        Me.Label2.Location = New System.Drawing.Point(116, 543)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(190, 23)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Jhon Patrick L. Santiago"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Silver
+        Me.Label3.Location = New System.Drawing.Point(169, 566)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 23)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "BSCpE 5-3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(425, 606)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Seven Segment Display"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Public WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Assignment 2 - Seven Segment Display/Seven Segment Display/Form1.vb
@@ -0,0 +1,115 @@
+﻿Public Class Form1
+    Private counter As Integer = 0
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If counter = 0 Then
+            Button1.BackColor = Color.Red
+            Button2.BackColor = Color.Red
+            Button3.BackColor = Color.Red
+            Button4.BackColor = Color.Black
+            Button5.BackColor = Color.Red
+            Button6.BackColor = Color.Red
+            Button7.BackColor = Color.Red
+        End If
+
+        If counter = 1 Then
+            Button1.BackColor = Color.Black
+            Button2.BackColor = Color.Black
+            Button3.BackColor = Color.Red
+            Button4.BackColor = Color.Black
+            Button5.BackColor = Color.Black
+            Button6.BackColor = Color.Red
+            Button7.BackColor = Color.Black
+        End If
+
+        If counter = 2 Then
+            Button1.BackColor = Color.Red
+            Button2.BackColor = Color.Black
+            Button3.BackColor = Color.Red
+            Button4.BackColor = Color.Red
+            Button5.BackColor = Color.Red
+            Button6.BackColor = Color.Black
+            Button7.BackColor = Color.Red
+        End If
+
+        If counter = 3 Then
+            Button1.BackColor = Color.Red
+            Button2.BackColor = Color.Black
+            Button3.BackColor = Color.Red
+            Button4.BackColor = Color.Red
+            Button5.BackColor = Color.Red
+            Button6.BackColor = Color.Red
+            Button7.BackColor = Color.Black
+        End If
+
+        If counter = 4 Then
+            Button1.BackColor = Color.Black
+            Button2.BackColor = Color.Red
+            Button3.BackColor = Color.Red
+            Button4.BackColor = Color.Red
+            Button5.BackColor = Color.Black
+            Button6.BackColor = Color.Red
+            Button7.BackColor = Color.Black
+        End If
+
+        If counter = 5 Then
+            Button1.BackColor = Color.Red
+            Button2.BackColor = Color.Red
+            Button3.BackColor = Color.Black
+            Button4.BackColor = Color.Red
+            Button5.BackColor = Color.Red
+            Button6.BackColor = Color.Red
+            Button7.BackColor = Color.Black
+        End If
+
+        If counter = 6 Then
+            Button1.BackColor = Color.Red
+            Button2.BackColor = Color.Red
+            Button3.BackColor = Color.Black
+            Button4.BackColor = Color.Red
+            Button5.BackColor = Color.Red
+            Button6.BackColor = Color.Red
+            Button7.BackColor = Color.Red
+        End If
+
+        If counter = 7 Then
+            Button1.BackColor = Color.Red
+            Button2.BackColor = Color.Black
+            Button3.BackColor = Color.Red
+            Button4.BackColor = Color.Black
+            Button5.BackColor = Color.Black
+            Button6.BackColor = Color.Red
+            Button7.BackColor = Color.Black
+        End If
+
+        If counter = 8 Then
+            Button1.BackColor = Color.Red
+            Button2.BackColor = Color.Red
+            Button3.BackColor = Color.Red
+            Button4.BackColor = Color.Red
+            Button5.BackColor = Color.Red
+            Button6.BackColor = Color.Red
+            Button7.BackColor = Color.Red
+        End If
+
+        If counter = 9 Then
+            Button1.BackColor = Color.Red
+            Button2.BackColor = Color.Red
+            Button3.BackColor = Color.Red
+            Button4.BackColor = Color.Red
+            Button5.BackColor = Color.Red
+            Button6.BackColor = Color.Red
+            Button7.BackColor = Color.Black
+            counter = 0
+        Else
+            counter += 1
+        End If
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = TimeSpan.FromSeconds(1).TotalMilliseconds
+        Timer1.Start()
+
+
+
+    End Sub
+End Class
